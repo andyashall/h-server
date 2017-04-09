a41595c1e2181dab853c006ad6a96c06af87c09f
andy <andy@ashall.co>
2017-04-09 17:27:25 +0100

    h-api 2.0

---
+++ app/Main.hs
@@ -0,0 +1,68 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveAnyClass #-}
+
+import Web.Scotty
+import Data.Aeson
+import Data.Aeson.Lens
+import Control.Lens
+import Data.Text (Text)
+import GHC.Generics
+import Network.Wai.Middleware.Static
+import qualified Data.ByteString.Lazy as B
+
+import Data.Monoid (mconcat)
+
+data Post = Post { _id :: Text
+                 , title :: Text
+                 , url :: Text
+                 , created :: Text
+                 , subject :: Text
+                 , summary :: Text
+                 , content :: Text
+                 , likes :: Int } deriving (Show, Generic)
+
+instance FromJSON Post
+instance ToJSON Post
+
+artJson :: [Post]
+artJson = [ Post { _id = "fhpzah1lCBUGt9dS4rhM", title = "Mlog - Meeting Action Tracker", url = "lorem-ipsum-dolor-sit-amet-1", created = "2017-03-28T12:42:24.915Z", subject = "JavaScript", summary = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", content = "Hello", likes = 1 }, Post { _id = "fhpzah1lCBUGt9dS4rhZ", title = "Mlog - Meeting Action Tracker", url = "lorem-ipsum-dolor-sit-amet-2", created = "2017-03-28T12:42:24.915Z", subject = "JavaScript", summary = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", content = "Hello", likes = 1 }, Post { _id = "fhpzah1lCBUGt9dS4rhh", title = "Another - Meeting Action Tracker", url = "lorem-ipsum-dolor-sit-amet", created = "2017-03-28T12:42:24.915Z", subject = "JavaScript", summary = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", content = "Hello", likes = 1 } ]
+
+projJson :: [Post]
+projJson = [ Post { _id = "fhpzah1lCBUGt9dS4rhM", title = "Woop - Meeting Action Tracker", url = "lorem-ipsum-dolor-sit-amet-2", created = "2017-03-28T12:42:24.915Z", subject = "JavaScript", summary = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", content = "Hello", likes = 1 }, Post { _id = "fhpzah1lCBUGt9dS4rhZ", title = "Mlog - Meeting Action Tracker", url = "lorem-ipsum-dolor-sit-amet-1", created = "2017-03-28T12:42:24.915Z", subject = "JavaScript", summary = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", content = "Hello", likes = 1 }, Post { _id = "fhpzah1lCBUGt9dS4rhh", title = "Another - Meeting Action Tracker", url = "lorem-ipsum-dolor-sit-amet", created = "2017-03-28T12:42:24.915Z", subject = "JavaScript", summary = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", content = "Hello", likes = 1 } ]
+
+-- projPath :: FilePath
+-- projPath = "projects.json"
+
+-- projJson :: IO B.ByteString
+-- projJson = B.readFile projPath
+
+staticServe :: ScottyM ()
+staticServe = do
+  middleware $ staticPolicy (addBase "./dist")
+
+matchesUrl :: Text -> Post -> Bool
+matchesUrl fUrl post = url post == fUrl
+
+-- getArticle url = do
+--   let filter = "url" :: Text
+--   let article = artJson ^? key filter == Just (String url)
+--   Web.Scotty.json $ article
+
+serve :: ScottyM ()
+serve = do
+  get "/api/getarticle" $ do
+    fUrl <- param "url"
+    Web.Scotty.json $ (filter (matchesUrl fUrl) artJson) !!0
+  get "/api/getproject" $ do
+    fUrl <- param "url"
+    -- let projects = (decode projJson) :: [Post]
+    Web.Scotty.json $ (filter (matchesUrl fUrl) projJson) !!0
+  get "/api/getarticles" $ do
+    Web.Scotty.json $ artJson
+  get "/api/getprojects" $ do
+    Web.Scotty.json $ projJson
+  get "" $ do
+    file $ "./dist/index.html"
+
+main = scotty 4000 (staticServe >> serve)
