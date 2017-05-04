49c458db51330c06b0b0e94597d9afe846f89f1f
andy <andy@ashall.co>
2017-05-04 21:08:36 +0100

    can an IO [Post] be parsed to json?

--- app/Main.hs
+++ app/Main.hs
@@ -17,6 +17,8 @@ import Data.Text.IO as D
 
 import Data.Monoid (mconcat)
 
+data Date = Date { date :: Text } deriving (Show, Generic)
+
 data Post = Post { _id :: Text
                  , title :: Text
                  , url :: Text
@@ -29,6 +31,9 @@ data Post = Post { _id :: Text
 instance FromJSON Post
 instance ToJSON Post
 
+instance FromJSON Date
+instance ToJSON Date
+
 artJson :: [Post]
 artJson = [ Post { _id = "fhpzah1lCBUGt9dS4rhM", title = "Mlog - Meeting Action Tracker", url = "lorem-ipsum-dolor-sit-amet-1", created = "2017-03-28T12:42:24.915Z", subject = "JavaScript", summary = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", content = "Hello", likes = 1 }, Post { _id = "fhpzah1lCBUGt9dS4rhZ", title = "Mlog - Meeting Action Tracker", url = "lorem-ipsum-dolor-sit-amet-2", created = "2017-03-28T12:42:24.915Z", subject = "JavaScript", summary = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", content = "Hello", likes = 1 }, Post { _id = "fhpzah1lCBUGt9dS4rhh", title = "Another - Meeting Action Tracker", url = "lorem-ipsum-dolor-sit-amet", created = "2017-03-28T12:42:24.915Z", subject = "JavaScript", summary = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", content = "Hello", likes = 1 } ]
 
@@ -38,8 +43,15 @@ projJson = [ Post { _id = "fhpzah1lCBUGt9dS4rhM", title = "Woop - Meeting Action
 jsonFile :: FilePath
 jsonFile = "projects.json"
 
-getJSON :: IO (B.ByteString)
-getJSON = B.readFile jsonFile
+getJSONFile :: IO (B.ByteString)
+getJSONFile = B.readFile jsonFile
+
+getJSON :: IO ([Post])
+getJSON = do
+  d <- (eitherDecode <$> getJSONFile) :: IO (Either String [Post])
+  case d of
+    Left err -> return projJson
+    Right ps -> return ps
 
 encodeJSON :: Maybe B.ByteString -> Maybe [Post]
 encodeJSON Nothing = Nothing
@@ -64,6 +76,10 @@ serve = do
     Web.Scotty.json $ artJson
   get "/api/getprojects" $ do
     Web.Scotty.json $ projJson
+    -- d <- (eitherDecode <$> getJSONFile) :: IO (Either String [Post])
+    -- case d of
+    --   Left err -> Web.Scotty.json $ projJson
+    --   Right ps -> Web.Scotty.json $ ps
   get "" $ do
     file $ "./dist/index.html"
   -- post "/api/likearticle" $ do
